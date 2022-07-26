70e6b0b8582ad0fa4dced3e6f1c043e1d9d97539
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-07-26 15:30:53 +0300

    2hw almost done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', saveTime);
+
+function saveTime({ seconds }) {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+let savedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(savedTime);
