f0f280484cd846ff8d2401604202848b6bfefb12
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-07-26 17:30:09 +0300

    3hw done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+updateOutput();
+
+refs.form.addEventListener('input', saveData);
+refs.form.addEventListener('submit', submitData);
+
+function saveData(event) {
+  const { email, message } = event.currentTarget;
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+
+function submitData(event) {
+  event.preventDefault();
+  const saveddata = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseddata = JSON.parse(saveddata);
+  console.log(parseddata);
+  localStorage.clear();
+  event.currentTarget.reset();
+}
+
+function updateOutput() {
+  const saveddata = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseddata = JSON.parse(saveddata);
+
+  if (parseddata) {
+    refs.input.value = parseddata.email;
+    refs.textarea.value = parseddata.message;
+  }
+}
