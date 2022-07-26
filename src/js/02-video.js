da67657cac916950a8f4948d75aefb667ab3fb33
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-07-26 15:43:19 +0300

    2hw done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,10 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', saveTime);
+player.on('timeupdate', throttle(saveTime, 1000));
 
 function saveTime({ seconds }) {
   console.log(seconds);
